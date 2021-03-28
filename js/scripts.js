954a89b6edd44dcba6baf25cae98174f8fba8899
justalittlemoh <maurice.stiller@hotmail.com>
2021-03-28 18:05:25 +0200

    version 1 complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,25 @@
 
 
+// code that runs, when page is loaded
 
-// onclick events homepage
+$(document).ready($('.rounded').addClass('shadow'));
+$(document).ready($('.carousel-item,.carousel-img').addClass('shadow'));
+$(document).ready($('#introductionModal').modal('show'));
+
+
+
+// onclick events
+
+$('.activateContact').click(function() {
+    $('#contactModal').modal('show');
+});
+
+$('#btnMailme').click(function() {
+    $('#messageForm').toggle('slow');
+    $('#btnMailme').attr("type","submit");
+    $('#btnMailme').text('Submit').click(function(){
+        $('#btnMailme').submit();
+        $('#introductionModal').modal('toggle');
+    });
+});
 
